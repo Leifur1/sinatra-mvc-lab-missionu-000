b671e9aaa8e12898938c650cbc5644ad8a22b4df
Leifur Sigurdsson <leifurerik@gmail.com>
2018-12-18 00:29:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/user_input' do
+    erb :user_input
+  end
+
+  post '/' do
+    @piglatin = PigLatinizer.new(input_str)
+    erb :user_input
+  end
 end
